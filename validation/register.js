9ea4ba821a93ef4ebab84f27aaebeae3f7ea3d96
“Evan <frommere@uchicago.edu>
2022-07-04 16:44:53 -0400

    user-auth backend

---
+++ validation/register.js
@@ -0,0 +1,25 @@
+const Validator = require("validator");
+const validText = require("./valid-text")
+module.exports = function validateRegisterInput(data) {
+    let errors = {};
+    data.email = validText(data.email) ? data.email:"";
+    data.password = validText(data.password) ? data.password:"";
+    data.password2 = validText(data.password2) ? data.password2:"";
+
+    if (Validator.isEmpty(data.email)) {
+        errors.email = "Email field is required"
+    }
+    if (Validator.isEmpty(data.password)) {
+        errors.password = "password is required"
+    }
+    if (!Validator.isLength(data.password,{min:2, max:30})) {
+        errors.password = "Password must be between 2 and 30 chars"
+    }
+    if (!Validator.equals(data.password, data.password2)) {
+        errors.password2 = "Passwords must match"
+    }
+    return {
+        errors,
+        isValid: Object.keys(errors).length===0
+    }
+}
