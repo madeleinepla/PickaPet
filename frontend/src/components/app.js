275e57d9b800143c19cff900fe4f0d67ec8b578e
dzhengg98 <97359918+dzhengg98@users.noreply.github.com>
2022-07-05 15:06:48 -0400

    Update app.js

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -5,12 +5,9 @@ import { Route } from 'react-router-dom';
 import HomePage from './home_page/home_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
-<<<<<<< HEAD
 import pet_index_container from './pets/pet_index_container';
-=======
 import '../styles/index.css'
 import ProfileContainer from './User/profile_container';
->>>>>>> main
 
 const App = () => (
   <Switch>
