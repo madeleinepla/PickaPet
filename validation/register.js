ccb65c7a4ee07288bf6ce7d119ddef38db3d5ed9
“Evan <frommere@uchicago.edu>
2022-07-05 09:49:16 -0400

    add username

--- validation/register.js
+++ validation/register.js
@@ -2,10 +2,17 @@ const Validator = require("validator");
 const validText = require("./valid-text")
 module.exports = function validateRegisterInput(data) {
     let errors = {};
+    data.username = validText(data.username) ? data.username:"";
     data.email = validText(data.email) ? data.email:"";
     data.password = validText(data.password) ? data.password:"";
     data.password2 = validText(data.password2) ? data.password2:"";
 
+    if (!Validator.isLength(data.username,{min:2, max:30})) {
+        errors.username = "Username must be between 2 and 30 chars";
+    }
+    if (Validator.isEmpty(data.username)) {
+        errors.username = "Username field is required"
+    }
     if (Validator.isEmpty(data.email)) {
         errors.email = "Email field is required"
     }
