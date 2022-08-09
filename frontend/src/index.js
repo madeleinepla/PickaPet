ab241671b99ba61ba0a921cb222ad6b30d80f592
nilshen <linshen88@gmail.com>
2022-08-09 13:00:59 -0400

    update for login error

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -23,7 +23,6 @@ document.addEventListener('DOMContentLoaded', () => {
     if (decodedUser.exp < currentTime) {
       store.dispatch(logout());
       window.location.href = '/';
-      // window.location.href = '/login';
     }
   } else {
     store = configureStore({});
